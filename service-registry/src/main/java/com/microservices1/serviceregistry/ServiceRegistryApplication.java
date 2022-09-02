70ede55fe3dea904e54fda8f5b147a98090abf23
unknown <mindc1jun175@iiht.tech>
2022-09-02 12:45:04 +0530

    backend

---
+++ service-registry/src/main/java/com/microservices1/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,17 @@
+package com.microservices1.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication{
+
+	public static void main(String[] args) throws Exception{
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+}
